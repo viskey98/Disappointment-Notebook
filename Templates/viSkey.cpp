1ffd0acda94076a9f35e323c422625978ff80c55
Viswak Hanumanth <viswakrocks@gmail.com>
2017-08-30 14:59:38 +0530

    viSkey template

---
+++ Templates/viSkey.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<bits/stdc++.h>
+#include<queue>
+#define FOR0(i,n) for(i=0;i<n;i++)
+#define FOR(i,j,n) for(i=j;i<n;i++)
+#define FORD(i,j,k) for(i=j;i>=k;i--)
+#define MAX2(a,b) (a)>(b)?(a):(b)
+#define MAX3(a,b,c) (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c))
+#define MIN2(a,b) (a)<(b)?(a):(b)
+#define MIN3(a,b,c) (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c))
+using namespace std;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<pair<int,int>> vii;
+int main()
+{
+ 
+}
