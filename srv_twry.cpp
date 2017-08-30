ed90cccd20b8a3c75679ee6295967cfb82e4af15
srv-twry <saurav007tiwary@gmail.com>
2017-08-30 14:45:16 +0530

    added my template

---
+++ srv_twry.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ABS(a) ((a < 0) ? ((-1)*(a)) : (a))
+#define F(i,a,b) for(int i = (int)(a); i < (int)(b); i++)
+#define RF(i,a,b) for(int i = (int)(a); i >= (int)(b); i--)
+#define MIN3(a,b,c) (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c))
+#define MAX(a,b) (a)>(b)?(a):(b)
+#define MIN2(a,b) (a)<(b)?(a):(b)
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<ii> vii; 
+typedef vector<int> vi;
+typedef long long ll;
+
+int main(){
+	std::ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+
+	return 0;
+}
