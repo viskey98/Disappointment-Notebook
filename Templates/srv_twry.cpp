5e1ebdf601084a9bd58a558a2d5e9c3474907265
srv-twry <saurav007tiwary@gmail.com>
2017-09-15 01:46:57 +0530

    Template update

--- Templates/srv_twry.cpp
+++ Templates/srv_twry.cpp
@@ -5,14 +5,24 @@
 #define MIN3(a,b,c) (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c))
 #define MAX(a,b) (a)>(b)?(a):(b)
 #define MIN2(a,b) (a)<(b)?(a):(b)
+#define INF 1000000000
+#define SIEVELIM 10000000
+#define MOD 1000000000+7
+#define EPS 1e-7
 using namespace std;
 typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
 typedef vector<ii> vii; 
 typedef vector<int> vi;
 typedef long long ll;
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
 
 int main(){
 	std::ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-
+	
+	
+	
 	return 0;
 }
