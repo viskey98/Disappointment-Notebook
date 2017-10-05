a6e270efa7c9b4cfb03482cdcf753c552300a4bc
Viswak Hanumanth <viswakrocks@gmail.com>
2017-10-05 23:14:46 +0530

    Update #3

--- Templates/viSkey.cpp
+++ Templates/viSkey.cpp
@@ -10,6 +10,7 @@
 #define FOR0(i,n) for(i=0;i<n;i++)
 #define FOR(i,j,n) for(i=j;i<n;i++)
 #define FORD(i,j,k) for(i=j;i>=k;i--)
+#define pb push_back
 inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
 inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
 inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
