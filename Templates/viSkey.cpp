4a900624c7f39874406cec3b03b5eb3ad12c0295
Viswak Hanumanth <viswakrocks@gmail.com>
2017-10-27 10:00:54 +0530

    Update #6

--- Templates/viSkey.cpp
+++ Templates/viSkey.cpp
@@ -1,17 +1,10 @@
-#include<iostream>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
-#include<vector>
-#include<set>
-#include<map>
 #include<bits/stdc++.h>
-#include<queue>
-#include<stack>
 #define FOR0(i,n) for(i=0;i<n;i++)
 #define FOR(i,j,n) for(i=j;i<n;i++)
 #define FORD(i,j,k) for(i=j;i>=k;i--)
 #define pb push_back
+#define ff first
+#define ss second
 inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
 inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
 inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
@@ -23,6 +16,4 @@ typedef vector<long long> vll;
 typedef vector< pair<int,int> > vii;
 typedef long long ll;
 int main()
-{
- 
-}
+{}
