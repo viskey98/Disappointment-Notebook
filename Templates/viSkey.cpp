96d60e11a5e2da8275f5fcdd3d776d6f6671cbab
Viswak Hanumanth <viswakrocks@gmail.com>
2017-12-30 18:56:08 +0530

    Update #8

--- Templates/viSkey.cpp
+++ Templates/viSkey.cpp
@@ -5,7 +5,6 @@
 #define pb push_back
 #define ff first
 #define ss second
-#define endl '\n'
 inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
 inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
 inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
@@ -17,6 +16,6 @@ typedef vector<long long> vll;
 typedef vector< pair<int,int> > vii;
 typedef long long ll;
 int main()
-{
-   std::ios::sync_with_stdio(false);	
+{	
+   
 }
