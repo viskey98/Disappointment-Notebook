e33d7ebb1a8a2a9198d43ba622562c87405923a2
Viswak Hanumanth <viswakrocks@gmail.com>
2019-08-07 10:29:38 +0530

    Update viSkey.cpp

--- Templates/viSkey.cpp
+++ Templates/viSkey.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
 #define FOR0(i,n) for(i=0;i<n;i++)
 #define FOR(i,j,n) for(i=j;i<n;i++)
 #define FORD(i,j,k) for(i=j;i>=k;i--)
@@ -10,6 +12,7 @@
 #define ninf -1000000000
 #define endl '\n' 
 #define she_told_me_dont_worry ios_base::sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0) ;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 // Use cout.flush() for interactive problems.
 // Use this for increased stack size: g++ -o a.exe -Wl,--stack=256000000 -O2 source.cpp
 inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
@@ -17,6 +20,7 @@ inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?(
 inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
 inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
 using namespace std;
+using namespace __gnu_pbds; 
 typedef long long ll;
 typedef pair<int,int> ii;
 typedef pair<ll, ll> pll;
